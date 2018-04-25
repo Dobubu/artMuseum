61779590c79de73fce986dee0c2c0a551320a246
dorothy <rilakkumadoro@gmail.com>
2018-04-25 16:17:04 +0800

    '載入插建及樣式，確定router'

---
+++ src/components/layout/footer.vue
@@ -0,0 +1,17 @@
+<template lang="html">
+	<div class="container-fluid bg-secondary text-dark">
+		<div class="container">
+			<div class="rol py-3">
+				<p class="mb-0">Copyright © 2018 - All Rights Reserved - Mischa</p>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ src/components/page/content.vue
@@ -0,0 +1,21 @@
+<template lang="html">
+    <div>
+        <appHeader/>
+        <appFooter/>
+    </div>
+</template>
+
+<script>
+import appHeader from '@/components/layout/header.vue'
+import appFooter from '@/components/layout/footer.vue'
+
+export default {
+    components: {
+      appHeader,
+      appFooter
+    },
+}
+</script>
+
+<style lang="css" scoped>
+</style>
